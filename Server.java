5226cb5fa02718848b71ae8caa525a0569cbe272
ferb120 <root@DESKTOP-26G29D7.localdomain>
2018-04-12 19:59:05 -0700

    Completed Server class and added Request class

--- Server.java
+++ Server.java
@@ -1,5 +1,30 @@
+import java.io.*;
+import java.net.*;
+
 class Server {
-    public static void main(String[] args) {
-        System.out.println("Hello World!"); // Display the string.
+    public void go() {
+        try {
+            ServerSocket serverSocket = new ServerSocket(4242);
+            
+            while (true){
+
+                Socket sock = serverSocket.accept();
+
+                PrintWriter writer = new PrintWriter(sock.getOutputStream());
+                String helloWorld = "Hello World";
+                writer.println(helloWorld);
+                writer.close();
+                System.out.println(helloWorld)
+            }
+        } catch (IOExecption ex){
+            ex.printStackTrace();
+        }
+    }
+
+
+    public static void main(String[] args){
+
+        Server server = new Server();
+        server.go();
     }
 }
